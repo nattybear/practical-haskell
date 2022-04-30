9b7112877b2d0e00d1db38a5525f4ceafa20dded
준규 <vpark45@gmail.com>
2022-04-30 22:51:49 +0900

    Rewrite maxmin using local bindings

--- src/part1/chapter2/app/Chapter2/SimpleFunctions.hs
+++ src/part1/chapter2/app/Chapter2/SimpleFunctions.hs
@@ -14,12 +14,11 @@ reverse2 list = if null list
                 else reverse2 (tail list) +++ [head list]
 
 maxmin :: Ord a => [a] -> (a, a)
-maxmin list = if null (tail list)
-              then (head list, head list)
-              else ( if (head list) > fst (maxmin (tail list))
-                     then head list
-                     else fst (maxmin (tail list))
-                   , if (head list) < snd (maxmin (tail list))
-                     then head list
-                     else snd (maxmin (tail list))
-                   )
+maxmin list = let h = head list
+              in if null (tail list)
+                 then (h, h)
+                 else ( if h > t_max then h else t_max
+                      , if h < t_min then h else t_min )
+                      where t = maxmin (tail list)
+                            t_max = fst t
+                            t_min = snd t
