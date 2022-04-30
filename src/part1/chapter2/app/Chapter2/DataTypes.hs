487d393f3683140aa7ee9cdd903ea6ebe2366d05
준규 <vpark45@gmail.com>
2022-04-30 23:11:02 +0900

    Rewrite Individual using Person type

--- src/part1/chapter2/app/Chapter2/DataTypes.hs
+++ src/part1/chapter2/app/Chapter2/DataTypes.hs
@@ -2,5 +2,8 @@ module Chapter2.DataTypes where
 
 data Client = GovOrg     String
             | Company    String Integer String String
-            | Individual String String Bool
+            | Individual Person Bool
+            deriving Show
+
+data Person = Person String String
             deriving Show
