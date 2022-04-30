970790a5ffb2120f151e05589be84dde89c352de
준규 <vpark45@gmail.com>
2022-04-30 23:08:49 +0900

    Add deriving Show

--- src/part1/chapter2/app/Chapter2/DataTypes.hs
+++ src/part1/chapter2/app/Chapter2/DataTypes.hs
@@ -3,3 +3,4 @@ module Chapter2.DataTypes where
 data Client = GovOrg     String
             | Company    String Integer String String
             | Individual String String Bool
+            deriving Show
