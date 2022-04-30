097350b2fec6e6a381bfaec3b8f851221942ece4
준규 <vpark45@gmail.com>
2022-04-30 22:32:43 +0900

    Add reverse2 function

--- src/part1/chapter2/app/Chapter2/SimpleFunctions.hs
+++ src/part1/chapter2/app/Chapter2/SimpleFunctions.hs
@@ -7,3 +7,8 @@ firstOrEmpty lst = if not (null lst) then head lst else "empty"
 lst1 +++ lst2 = if null lst1  {- check emptyness -}
                 then lst2      -- base case
                 else (head lst1) : (tail lst1 +++ lst2)
+
+reverse2 :: [a] -> [a]
+reverse2 list = if null list
+                then []
+                else reverse2 (tail list) +++ [head list]
