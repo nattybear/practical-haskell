6835f9462812f1d54dd84bb9846505ed390d80fa
준규 <vpark45@gmail.com>
2022-05-01 22:41:35 +0900

    Add discounts function and fix TimeMachine data type

--- src/part1/chapter2/app/Chapter2/DataTypes.hs
+++ src/part1/chapter2/app/Chapter2/DataTypes.hs
@@ -11,7 +11,14 @@ data Person = Person String String Gender
 data Gender = Male | Female | Unknown
             deriving Show
 
-data TimeMachine = String Integer String Time Double
+-- Time machines are defined by
+-- their manufacturer,
+-- their model (which is an integer),
+-- their name,
+-- whether they can travel to the past and to the future,
+-- and a price
+-- (which can be represented as a floating-point number)
+data TimeMachine = TimeMachine String Integer String Time Double
 
 data Time = Past | Future
 
@@ -32,3 +39,11 @@ numberOfClient (GovOrg _)                         = Nothing
 numberOfClient (Company _ _ _ _)                  = Nothing
 numberOfClient (Individual (Person _ _ gender) _) =
   Just (NumberOfClient gender 1)
+
+discount :: Double -> TimeMachine -> TimeMachine
+discount percent (TimeMachine manufacturer model name time price) =
+  TimeMachine manufacturer model name time newPrice
+  where newPrice = price * (1 - percent)
+
+discounts :: Double -> [TimeMachine] -> [TimeMachine]
+discounts percent = map (discount percent)
