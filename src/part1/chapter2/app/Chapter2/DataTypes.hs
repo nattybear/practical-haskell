7d1255846eecf7bbccbe81e6807379e324255720
준규 <vpark45@gmail.com>
2022-05-01 22:15:30 +0900

    Pattern match on some parameter to a function

--- src/part1/chapter2/app/Chapter2/DataTypes.hs
+++ src/part1/chapter2/app/Chapter2/DataTypes.hs
@@ -16,12 +16,10 @@ data TimeMachine = String Integer String Time Double
 data Time = Past | Future
 
 clientName :: Client -> String
-clientName client = case client of
-                      GovOrg     name       -> name
-                      Company    name _ _ _ -> name
-                      Individual (Person fNm lNm _) _ -> fNm ++ " " ++ lNm
+clientName (GovOrg name)                     = name
+clientName (Company name _ _ _)              = name
+clientName (Individual (Person fNm lNm _) _) = fNm ++ " " ++ lNm
 
 companyName :: Client -> Maybe String
-companyName client = case client of
-                       Company name _ _ _ -> Just name
-                       _                  -> Nothing
+companyName (Company name _ _ _) = Just name
+companyName _                    = Nothing
