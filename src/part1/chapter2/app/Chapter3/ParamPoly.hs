0c9aa631346a14010bf6b96cb411726ae021030a
준규 <vpark45@gmail.com>
2022-05-04 22:53:26 +0900

    Functions As Parameters

--- src/part1/chapter2/app/Chapter3/ParamPoly.hs
+++ src/part1/chapter2/app/Chapter3/ParamPoly.hs
@@ -11,3 +11,16 @@ data Client i = GovOrg  { clientId :: i, clientName :: String }
 
 data Person = Person { firstName :: String, lastName :: String }
               deriving (Show, Eq, Ord)
+
+swapTriple (x,y,z) = (y,z,x)
+
+duplicate x = (x,x)
+
+nothing _ = Nothing
+
+index [] = []
+index [x] = [(0, x)]
+index (x:xs) = let indexed@((n,_):_) = index xs
+               in  (n+1,x):indexed
+
+maybeA [] = 'a'
