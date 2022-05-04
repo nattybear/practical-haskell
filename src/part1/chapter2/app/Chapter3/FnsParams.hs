90d179ca500812de6febdaa22f83c3baa68e2e82
준규 <vpark45@gmail.com>
2022-05-05 03:38:52 +0900

    Exercise 3-2. Working with filters

--- src/part1/chapter2/app/Chapter3/FnsParams.hs
+++ src/part1/chapter2/app/Chapter3/FnsParams.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE LambdaCase #-}
 module Chapter3.FnsParams where
 
+import Chapter2.DataTypes
+
 apply3f2 :: (Integer -> Integer) -> Integer -> Integer
 apply3f2 f x = 3 * f (x + 2)
 
@@ -14,3 +16,17 @@ sayHello names = map (\case "Alejandro" -> "Hello, writer"
 
 multiplyByN :: Integer -> (Integer -> Integer)
 multiplyByN n = \x -> n*x
+
+filterOnes :: (Eq a, Num a) => [a] -> [a]
+filterOnes = filter (== 1)
+
+filterANumber :: (Eq a, Num a) => a -> [a] -> [a]
+filterANumber n = filter (== n)
+
+filterNot :: (a -> Bool) -> [a] -> [a]
+filterNot f = filter (not . f)
+
+filterGovOrgs :: [Client] -> [Client]
+filterGovOrgs = filter isGovOrg
+  where isGovOrg = \case GovOrg _ -> True
+                         client   -> False
