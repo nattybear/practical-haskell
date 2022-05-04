1bcf08de5d882bfe80129ec56ab3a25f2ec51d7a
준규 <vpark45@gmail.com>
2022-05-04 22:33:40 +0900

    Add Client and Person data type

--- src/part1/chapter2/app/Chapter3/ParamPoly.hs
+++ src/part1/chapter2/app/Chapter3/ParamPoly.hs
@@ -2,3 +2,12 @@ module Chapter3.ParamPoly where
 
 maybeString (Just _) = "Just"
 maybeString Nothing  = "Nothing"
+
+data Client i = GovOrg  { clientId :: i, clientName :: String }
+              | Company { clientId :: i, clientName :: String
+                        , person :: Person, duty :: String }
+              | Individual { clientId :: i, person :: Person }
+              deriving (Show, Eq, Ord)
+
+data Person = Person { firstName :: String, lastName :: String }
+              deriving (Show, Eq, Ord)
