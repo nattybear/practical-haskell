e979d92834b1bca08855dd60a1a6a98878a3b728
준규 <vpark45@gmail.com>
2022-05-06 07:40:12 +0900

    Add range function

--- src/part1/chapter2/app/Chapter3/Ranges.hs
+++ src/part1/chapter2/app/Chapter3/Ranges.hs
@@ -1,3 +1,6 @@
 module Chapter3.Ranges where
 
 data Range = Range Integer Integer deriving Show
+
+range :: Integer -> Integer -> Range
+range a b = if a <= b then Range a b else error "a must be <= b"
