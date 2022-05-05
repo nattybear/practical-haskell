8aae07fe8dc0022832554ae9b6f533cbfdc133f4
준규 <vpark45@gmail.com>
2022-05-06 07:47:50 +0900

    Add the observation data type and the conversion function

--- src/part1/chapter2/app/Chapter3/Ranges.hs
+++ src/part1/chapter2/app/Chapter3/Ranges.hs
@@ -4,3 +4,8 @@ data Range = Range Integer Integer deriving Show
 
 range :: Integer -> Integer -> Range
 range a b = if a <= b then Range a b else error "a must be <= b"
+
+data RangeObs = R Integer Integer deriving Show
+
+r :: Range -> RangeObs
+r (Range a b) = R a b
