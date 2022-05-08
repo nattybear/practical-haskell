3e48d54acd9d0ec1b200f48dc82e823bac2fd50b
준규 <vpark45@gmail.com>
2022-05-08 13:49:29 +0900

    Add infMax function

--- src/part1/chapter2/app/Chapter3/Lists.hs
+++ src/part1/chapter2/app/Chapter3/Lists.hs
@@ -4,3 +4,10 @@ data InfNumber a = MinusInfinity
                  | Number a
                  | PlusInfinity
                  deriving Show
+
+infMax :: Ord a => InfNumber a -> InfNumber a -> InfNumber a
+infMax MinusInfinity x             = x
+infMax x             MinusInfinity = x
+infMax PlusInfinity  _             = PlusInfinity
+infMax _             PlusInfinity  = PlusInfinity
+infMax (Number a)    (Number b)    = Number (max a b)
