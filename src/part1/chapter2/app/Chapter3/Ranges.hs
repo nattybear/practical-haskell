63a14c77660c3f21e26b42611c1ed505d2f0b137
준규 <vpark45@gmail.com>
2022-05-08 13:21:25 +0900

    Rewrite R using PatternSynonyms

--- src/part1/chapter2/app/Chapter3/Ranges.hs
+++ src/part1/chapter2/app/Chapter3/Ranges.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE PatternSynonyms #-}
 module Chapter3.Ranges (Range(), range) where
 
 data Range = Range Integer Integer deriving Show
@@ -6,11 +6,6 @@ data Range = Range Integer Integer deriving Show
 range :: Integer -> Integer -> Range
 range a b = if a <= b then Range a b else error "a must be <= b"
 
-data RangeObs = R Integer Integer deriving Show
-
-r :: Range -> RangeObs
-r (Range a b) = R a b
-
-prettyRange :: Range -> String
-prettyRange rng = case rng of
-                    (r -> R a b) -> "[" ++ show a ++ "," ++ show b ++ "]"
+pattern R :: Integer -> Integer -> Range
+pattern R a b <- Range a b
+  where R a b = range a b
