948697fe8a92d6882aee4d4b488acda722ee4e60
준규 <vpark45@gmail.com>
2022-05-10 07:28:01 +0900

    Add listOfClients

--- src/part1/chapter2/app/Chapter3/Lists.hs
+++ src/part1/chapter2/app/Chapter3/Lists.hs
@@ -58,3 +58,11 @@ compareClient (Individual{person = p1}) (Individual{person = p2})
 compareClient (Individual {}) _ = GT
 compareClient _ (Individual {}) = LT
 compareClient c1 c2             = compare (clientName c1) (clientName c2)
+
+listOfClients
+  = [ Individual 2 (Person "H. G." "Wells")
+    , GovOrg 3 "NTTF"  -- National Time Travel Foundation
+    , Company 4 "Wormhole Inc." (Person "Karl" "Schwarzschild") "Physicist"
+    , Individual 5 (Person "Doctor" "")
+    , Individual 6 (Person "Sarah" "Jane")
+    ]
