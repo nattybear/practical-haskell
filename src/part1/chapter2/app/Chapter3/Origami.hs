66436f11d5ee6bd9432fd74d8cd37146a8bee6c6
준규 <vpark45@gmail.com>
2022-05-10 21:05:50 +0900

    Add enumUnfold function

--- src/part1/chapter2/app/Chapter3/Origami.hs
+++ src/part1/chapter2/app/Chapter3/Origami.hs
@@ -1,7 +1,14 @@
 module Chapter3.Origami where
 
+import Data.List
+
 filterAsFold :: (a -> Bool) -> [a] -> [a]
 filterAsFold p = foldr (\x l -> if p x then x : l else l) []
 
 mapAsFold :: (a -> b) -> [a] -> [b]
 mapAsFold f = foldr (\x l -> f x : l) []
+
+enumUnfold :: Int -> Int -> [Int]
+enumUnfold n m = unfoldr (\x -> if x > m
+                                then Nothing
+                                else Just (x, x+1)) n
