dfb3b2ae5ea665e46aa8d98662c4f3681db7fb14
준규 <vpark45@gmail.com>
2022-05-10 21:13:42 +0900

    Add foldr2 and mapAsFold2 functions

--- src/part1/chapter2/app/Chapter3/Origami.hs
+++ src/part1/chapter2/app/Chapter3/Origami.hs
@@ -18,3 +18,11 @@ enumUnfold n m = unfoldr (\x -> if x > m
 minSort :: [Integer] -> [Integer]
 minSort = unfoldr (\case [] -> Nothing
                          xs -> Just (m, delete m xs) where m = minimum xs)
+
+foldr2 :: (Maybe (a,b) -> b) -> [a] -> b
+foldr2 f []     = f Nothing
+foldr2 f (x:xs) = f $ Just (x, foldr2 f xs)
+
+mapAsFold2 :: (a -> b) -> [a] -> [b]
+mapAsFold2 f = foldr2 (\case Nothing     -> []
+                             Just (x,xs) -> f x : xs)
