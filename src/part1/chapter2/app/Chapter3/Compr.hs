6c0cd735f3b487337030c6ac9fab726558df6f7b
준규 <vpark45@gmail.com>
2022-05-10 20:34:46 +0900

    Add companyAnalytics function

--- src/part1/chapter2/app/Chapter3/Compr.hs
+++ src/part1/chapter2/app/Chapter3/Compr.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE TransformListComp #-}
+{-# LANGUAGE RecordWildCards   #-}
+
 module Chapter3.Compr where
 
 import Chapter3.Lists
 import Chapter3.ParamPoly
+import GHC.Exts
 
 enum :: Int -> Int -> [Int]
 enum a b | a > b = []
@@ -12,3 +16,11 @@ withPositions list = zip [1 .. length list] list
 
 duplicateOdds :: [Integer] -> [Integer]
 duplicateOdds list = [ 2 * x | x <- list, odd x ]
+
+companyAnalytics :: [Client a] -> [(String, [(Person, String)])]
+companyAnalytics clients = [ (the clientName, zip person duty)
+                           | client@(Company { .. }) <- clients
+                           , then sortWith by duty
+                           , then group by clientName using groupWith
+                           , then sortWith by length client
+                           ]
