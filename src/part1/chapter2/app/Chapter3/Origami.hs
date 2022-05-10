afc80bff066235cdda47cd0954ed5187749d7ee4
준규 <vpark45@gmail.com>
2022-05-10 20:52:36 +0900

    Add mapAsFold function

--- src/part1/chapter2/app/Chapter3/Origami.hs
+++ src/part1/chapter2/app/Chapter3/Origami.hs
@@ -2,3 +2,6 @@ module Chapter3.Origami where
 
 filterAsFold :: (a -> Bool) -> [a] -> [a]
 filterAsFold p = foldr (\x l -> if p x then x : l else l) []
+
+mapAsFold :: (a -> b) -> [a] -> [b]
+mapAsFold f = foldr (\x l -> f x : l) []
