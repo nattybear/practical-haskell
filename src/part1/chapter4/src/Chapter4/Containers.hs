71e5cdca2ae854e9c8f5982db5576cebc6d83484
Vincent <vpark45@gmail.com>
2022-05-18 22:41:47 +0000

    Add ClientKind type

--- src/part1/chapter4/src/Chapter4/Containers.hs
+++ src/part1/chapter4/src/Chapter4/Containers.hs
@@ -1,9 +1,8 @@
 {-# LANGUAGE LambdaCase #-}
 module Chapter4.Containers where
 
+import Chapter3.ParamPoly
 import qualified Data.Map as M
-
--- M.alter :: Ord k => (Maybe a -> Maybe a) -> k -> M.Map k a -> M.Map k a
 
 insert :: Ord k => k -> a -> M.Map k a -> M.Map k a
 insert k v = M.alter (\_ -> Just v) k
@@ -14,3 +13,5 @@ delete = M.alter (\_ -> Nothing)
 adjust :: Ord k => (a -> a) -> k -> M.Map k a -> M.Map k a
 adjust f = M.alter (\case Nothing -> Nothing
                           Just v  -> Just (f v))
+
+data ClientKind = GovOrgKind | CompanyKind | IndividualKind
