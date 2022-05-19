ed0fb9d6bdd662957bff064bf93db0d3bfd8ed07
Vincent <vpark45@gmail.com>
2022-05-19 15:25:28 +0000

    Implement classifyClients function

--- src/part1/chapter4/src/Chapter4/Containers.hs
+++ src/part1/chapter4/src/Chapter4/Containers.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE RecordWildCards #-}
+
 module Chapter4.Containers where
 
 import Chapter3.ParamPoly
 import qualified Data.Map as M
+import qualified Data.Set as S
 
 insert :: Ord k => k -> a -> M.Map k a -> M.Map k a
 insert k v = M.alter (\_ -> Just v) k
@@ -14,4 +17,20 @@ adjust :: Ord k => (a -> a) -> k -> M.Map k a -> M.Map k a
 adjust f = M.alter (\case Nothing -> Nothing
                           Just v  -> Just (f v))
 
-data ClientKind = GovOrgKind | CompanyKind | IndividualKind
+data ClientKind = GovOrgKind
+                | CompanyKind
+                | IndividualKind
+                deriving (Eq, Ord, Show)
+
+classifyClients :: [Client Integer]
+                -> M.Map ClientKind (S.Set (Client Integer))
+classifyClients clients = go clients m'
+  where go (x:xs) m =
+          case x of
+            GovOrg     { .. } -> M.adjust (S.insert x) GovOrgKind     m
+            Company    { .. } -> M.adjust (S.insert x) CompanyKind    m
+            Individual { .. } -> M.adjust (S.insert x) IndividualKind m
+        m' = M.fromList [ (GovOrgKind,     s)
+                        , (CompanyKind,    s)
+                        , (IndividualKind, s) ]
+        s  = S.empty
