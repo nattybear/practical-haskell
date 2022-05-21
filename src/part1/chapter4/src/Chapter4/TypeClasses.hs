a6d81bdc54752c3eedd84eb1f91650b196878735
Vincent <vpark45@gmail.com>
2022-05-21 12:45:12 +0000

    Add Priceable type class

--- src/part1/chapter4/src/Chapter4/TypeClasses.hs
+++ src/part1/chapter4/src/Chapter4/TypeClasses.hs
@@ -1,7 +1,18 @@
 module Chapter4.TypeClasses where
 
+import Chapter2.DataTypes (TimeMachine)
+import Chapter3.ParamPoly
+
 class Nameable n where
   name :: n -> String
 
 initial :: Nameable n => n -> Char
 initial n = head (name n)
+
+instance Nameable (Client i) where
+  name Individual { person = Person { firstName = f, lastName = n } }
+    = f ++ " " ++ n
+  name c = clientName c
+
+class Priceable p where
+  totalPrice :: [p] -> Double
