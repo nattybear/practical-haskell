9cf77193dc25c48d18cfdae9370e8fc3c38b1a36
Vincent <vpark45@gmail.com>
2022-06-04 10:17:48 +0000

    Add sumForce function

--- src/part1/chapter5/src/Chapter5/Problems.hs
+++ src/part1/chapter5/src/Chapter5/Problems.hs
@@ -1,1 +1,7 @@
 module Chapter5.Problems where
+
+sumForce :: [Integer] -> Integer
+sumForce xs = sumForce' xs 0
+  where sumForce' [] z = z
+        sumForce' (y:ys) z = let s = z + y
+                             in  s `seq` sumForce' ys s
