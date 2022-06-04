7d62525a0535f4fb65bd9ec0317818bbba0eb31f
Vincent <vpark45@gmail.com>
2022-06-04 10:21:56 +0000

    Add sumYears function

--- src/part1/chapter5/src/Chapter5/Problems.hs
+++ src/part1/chapter5/src/Chapter5/Problems.hs
@@ -1,7 +1,17 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Chapter5.Problems where
+
+import Chapter5.Infinite
 
 sumForce :: [Integer] -> Integer
 sumForce xs = sumForce' xs 0
   where sumForce' [] z = z
         sumForce' (y:ys) z = let s = z + y
                              in  s `seq` sumForce' ys s
+
+sumYears :: [TimeMachine] -> Integer
+sumYears xs = sumYears' xs 0
+  where sumYears' []            z = z
+        sumYears' (TM _ !y :ys) z = let !s = z + y
+                                    in sumYears' ys s
