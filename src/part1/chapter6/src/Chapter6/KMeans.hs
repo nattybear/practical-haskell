be376fd110b16be6f0f6a239a17bbfaeb6fc5766
Vincent <vpark45@gmail.com>
2022-06-24 15:41:43 +0000

    Add Vector instance for tuple of Double

--- src/part1/chapter6/src/Chapter6/KMeans.hs
+++ src/part1/chapter6/src/Chapter6/KMeans.hs
@@ -1,4 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Chapter6.KMeans where
 
 class Vector v where
   distance :: v -> v -> Double
+
+instance Vector (Double, Double) where
+  distance (a,b) (c,d) = sqrt $ (c-a)*(c-a) + (d-b)*(d-b)
