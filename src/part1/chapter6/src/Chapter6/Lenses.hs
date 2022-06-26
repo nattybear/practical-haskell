d5f4299f42008ea428a22372dc5f837921837e3f
Vincent <vpark45@gmail.com>
2022-06-26 00:02:21 +0000

    Add firstName lens

--- src/part1/chapter6/src/Chapter6/Lenses.hs
+++ src/part1/chapter6/src/Chapter6/Lenses.hs
@@ -1,7 +1,13 @@
 module Chapter6.Lenses where
 
+import Lens.Micro.Platform
+
 data Client i = GovOrg     i String
               | Company    i String Person String
               | Individual i Person
 
 data Person = Person String String
+
+firstName :: Lens' Person String
+firstName = lens (\(Person f _) -> f)
+                 (\(Person _ l) newF -> Person newF l)
